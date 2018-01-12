750ac80e20c959d740cb0eada7823180262dc355
Filipe DA COSTA <filipe@dacosta.lu>
2018-01-12 16:19:22 +0100

    [BUGFIX] Moved body tag creation outside of COA/if statement

--- Configuration/TypoScript/Setup/Tracking/googleTagManager.ts
+++ Configuration/TypoScript/Setup/Tracking/googleTagManager.ts
@@ -18,10 +18,12 @@ page.jsInline.654.15 {
 
 page.bodyTagCObject = COA
 page.bodyTagCObject {
+
+	1 = TEXT
+	1.value = <body>
+	
 	654 = COA
 	654 {
-		10 = TEXT
-		10.value = <body>
 
 		20 = TEXT
 		20 {
